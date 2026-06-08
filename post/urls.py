612365f6199adbd5793dbe3ee396f1c2edc17e51
Isak <isakkarimov369@gmail.com>
2026-06-08 17:46:56 +0500

    hello

--- post/urls.py
+++ post/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import post_list_view
+from .views import post_list_view, post_tesha_view
 
 urlpatterns = [
-    path('', post_list_view, name='post_list')
+    path('', post_list_view, name='post_list'),
+    path('tesha/', post_tesha_view, name='post_tesha'),
 ]
--- post/views.py
+++ post/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def post_list_view(request):
     return render(request, "index.html")
+
+def post_tesha_view(request):
+    return render(request, "tesha.html")
